96573d7cedb1c3c32f69c99e3924c580f242cef0
Анатолий Журавлев <azhu@1c.ru>
2026-01-27 19:22:05 +0300

    Перенос актуальной обработки без внедрения УИ_ функций

--- build_actions.bat
+++ build_actions.bat
@@ -1,5 +1,7 @@
 chcp 65001
-SET TOOLS_UI_1C_BUILDER_EDT_PATH="C:\Program Files\1C\1CE\components\1c-edt-2024.1.3+13-x86_64\"
-SET TOOLS_UI_1C_BUILDER_PLATFORM_PATH="C:\Program Files\1cv8\8.3.14.2095\bin\"
+SET TOOLS_UI_1C_BUILDER_EDT_PATH=C:\Program Files\1C\1CE\components\1c-edt-2025.1.5+34-x86_64
+SET TOOLS_UI_1C_BUILDER_PLATFORM_PATH=C:\Program Files\1cv8\8.3.27.1964\bin
 
-oscript.exe -encoding=utf-8 .\src\builder\build.os 
+oscript.exe -encoding=utf-8 .\src\builder\build.os xml
+oscript.exe -encoding=utf-8 .\src\builder\build.os epf
+
