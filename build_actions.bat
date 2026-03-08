665f856799fdbb9fee1c3ceb62a13d2a24c8145c
Анатолий Журавлев <azhu@1c.ru>
2026-03-09 00:09:05 +0300

    Первая часть доработок редактора реквизитов объекта

--- build_actions.bat
+++ build_actions.bat
@@ -3,5 +3,5 @@ SET TOOLS_UI_1C_BUILDER_EDT_PATH=C:\Program Files\1C\1CE\components\1c-edt-2025.
 SET TOOLS_UI_1C_BUILDER_PLATFORM_PATH=C:\Program Files\1cv8\8.3.27.1964\bin
 
 oscript.exe -encoding=utf-8 .\src\builder\build.os xml
-oscript.exe -encoding=utf-8 .\src\builder\build.os epf
-
+::oscript.exe -encoding=utf-8 .\src\builder\build.os epf
+oscript.exe -encoding=utf-8 .\src\builder\build.os cfe
