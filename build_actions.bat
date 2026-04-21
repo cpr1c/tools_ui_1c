d2348e91476172538ee0fc2c1032e323688497e3
Анатолий Журавлев <azhu@1c.ru>
2026-04-22 01:36:40 +0300

    Третья часть доработок редактора реквизитов объекта

--- build_actions.bat
+++ build_actions.bat
@@ -1,7 +1,7 @@
 chcp 65001
-SET TOOLS_UI_1C_BUILDER_EDT_PATH=C:\Program Files\1C\1CE\components\1c-edt-2025.1.5+34-x86_64
-SET TOOLS_UI_1C_BUILDER_PLATFORM_PATH=C:\Program Files\1cv8\8.3.27.1964\bin
+SET TOOLS_UI_1C_BUILDER_EDT_PATH=C:\Users\zhuravlev_a\AppData\Local\1C\1cedtstart\installations\1C_EDT 2025.2\1cedt
+SET TOOLS_UI_1C_BUILDER_PLATFORM_PATH=C:\Program Files\1cv8\8.3.27.1989\bin
 
 oscript.exe -encoding=utf-8 .\src\builder\build.os xml
-::oscript.exe -encoding=utf-8 .\src\builder\build.os epf
+oscript.exe -encoding=utf-8 .\src\builder\build.os epf
 oscript.exe -encoding=utf-8 .\src\builder\build.os cfe
