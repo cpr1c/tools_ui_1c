dc7159582b23efbe14dec726fa753a2c3d88dbf8
Анатолий Журавлев <azhu@1c.ru>
2026-04-22 12:23:22 +0300

    Вернул build_actions.bat в старое значение
    Вернул build_actions.bat в старое значение

--- build_actions.bat
+++ build_actions.bat
@@ -1,7 +1,5 @@
 chcp 65001
-SET TOOLS_UI_1C_BUILDER_EDT_PATH=C:\Users\zhuravlev_a\AppData\Local\1C\1cedtstart\installations\1C_EDT 2025.2\1cedt
-SET TOOLS_UI_1C_BUILDER_PLATFORM_PATH=C:\Program Files\1cv8\8.3.27.1989\bin
+SET TOOLS_UI_1C_BUILDER_EDT_PATH="C:\Program Files\1C\1CE\components\1c-edt-2024.1.3+13-x86_64\"
+SET TOOLS_UI_1C_BUILDER_PLATFORM_PATH="C:\Program Files\1cv8\8.3.14.2095\bin\"
 
-oscript.exe -encoding=utf-8 .\src\builder\build.os xml
-oscript.exe -encoding=utf-8 .\src\builder\build.os epf
-oscript.exe -encoding=utf-8 .\src\builder\build.os cfe
+oscript.exe -encoding=utf-8 .\src\builder\build.os 
